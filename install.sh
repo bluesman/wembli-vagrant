ac4f32cbd93d97b1fa1fd963f7864b0c585cfe0a
Tom Walpole <tom@tomwalpole.com>
2013-12-24 12:40:48 -0800

    added vagrant up

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ git clone https://github.com/bluesman/express-meta-tags.git
 git clone https://github.com/bluesman/wembli-nginx.git nginx
 git clone https://github.com/bluesman/wembli-website.git website
 
+vagrant up
