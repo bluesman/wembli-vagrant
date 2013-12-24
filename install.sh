3ac757f8cd1469b769ef933c919e6341a36c6916
Tom Walpole <tom@tomwalpole.com>
2013-12-24 12:29:36 -0800

    install script

---
+++ install.sh
@@ -0,0 +1,6 @@
+mkdir src
+cd src
+git clone https://github.com/bluesman/express-meta-tags.git
+git clone https://github.com/bluesman/wembli-nginx.git nginx
+git clone https://github.com/bluesman/wembli-website.git website
+
